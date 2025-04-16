34ad972d34fb380bb3808c654bfa3990f76a72b6
Pujan5555 <puzanchakma@gmail.com>
2025-04-17 00:32:32 +0600

    a modified commit

--- blog.js
+++ blog.js
@@ -1,14 +1,3 @@
 document.getElementById("blog-btn").addEventListener("click", function () {
     window.location.href = "index.html";
 });
-const toggles = document.querySelectorAll('.accordion-toggle');
-
-toggles.forEach((toggle) => {
-  toggle.addEventListener('change', () => {
-    if (toggle.checked) {
-      toggles.forEach((other) => {
-        if (other !== toggle) other.checked = false;
-      });
-    }
-  });
-});
